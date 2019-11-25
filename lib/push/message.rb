1dde0bfa568220e371586bdf766603eddbf10153
Gwynant Jones <gwynantj@users.noreply.github.com>
2019-11-25 10:06:18 +0000

    [Hotfix] - Remove deliver after from ready_for_delivery scope (#3)
    * [Hotfix] - Remove deliver_after from the ready_for_delivery scope
    
    * Fix scope

--- lib/push/message.rb
+++ lib/push/message.rb
@@ -1,31 +1,34 @@
 require 'active_record'
 require 'active_record/errors'
 require 'push/daemon/database_reconnectable'
+
 module Push
   class Message < ActiveRecord::Base
     include Push::Daemon::DatabaseReconnectable
-    self.table_name = "push_messages"
+    self.table_name = 'push_messages'
 
-    validates :app, :presence => true
-    validates :device, :presence => true
+    validates :app, presence: true
+    validates :device, presence: true
 
-    scope :ready_for_delivery, -> { where('delivered = ? AND failed = ? AND (deliver_after IS NULL OR deliver_after < ?)', false, false, Time.now) }
+    scope :ready_for_delivery, -> {
+      where('delivered = ? AND failed = ?', false, false)
+    }
 
     def deliver(connection)
       begin
-        connection.write(self.to_message)
+        connection.write(to_message)
         check_for_error(connection)
 
         # this makes no sense in the rails environment, but it does in the daemon
         with_database_reconnect_and_retry(connection.name) do
           self.delivered = true
           self.delivered_at = Time.now
-          self.save!(:validate => false)
+          save!(validate: false)
         end
 
         Push::Daemon.logger.info("[#{connection.name}] Message #{id} delivered to #{device}")
-      rescue Push::DeliveryError, Push::DisconnectionError => error
-        handle_delivery_error(error, connection)
+      rescue Push::DeliveryError, Push::DisconnectionError => e
+        handle_delivery_error(e, connection)
         raise
       end
     end
@@ -41,7 +44,7 @@ module Push
         self.failed_at = Time.now
         self.error_code = error.code
         self.error_description = error.description
-        self.save!(:validate => false)
+        save!(validate: false)
       end
     end
   end
